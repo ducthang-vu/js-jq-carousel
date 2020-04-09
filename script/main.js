df6466b273c40c0432efd8cf2c33bb5c50af87ca
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-09 19:38:40 +0200

    major fixing

--- script/main.js
+++ script/main.js
@@ -1,57 +1,42 @@
-console.log('main.js is working')
-console.log($)
+$(document).ready (function() {
+
+    console.log('main.js is working')
+    console.log($)
 
 
-/***************/
-/*  FUNCTIONS  */
-/***************/
+    /***************/
+    /*  FUNCTIONS  */
+    /***************/
 
-function goNext() {
-    var currentActive = $('.img-box.active')
+    function goNext() {
+        var currentActive = $('.img-box.active')
+            currentActive.removeClass('active')
+            if (currentActive.hasClass('first')) {
+                $('.img-box.last').addClass('active')
+                $(rad_inputs[$('.img-box.last').attr('value')]).prop("checked", true)
+            }
+            else {
+                currentActive.prev().addClass('active')
+                $(rad_inputs[currentActive.prev().attr('value')]).prop("checked", true)
+            }
+    }
+
+
+    function goBack() {
+        var currentActive = $('.img-box.active')
         currentActive.removeClass('active')
-        if (currentActive.hasClass('first')) {
-            $('.img-box.last').addClass('active')
-            $(rad_inputs[$('.img-box.last').attr('value')]).prop("checked", true)
+        if (currentActive.hasClass('last')) {
+            $('.img-box.first').addClass('active')
+            $(rad_inputs[$('.img-box.first').attr('value')]).prop("checked", true)
         }
         else {
-            currentActive.prev().addClass('active')
-            $(rad_inputs[currentActive.prev().attr('value')]).prop("checked", true)
-        }
-}
-
-
-function goBack() {
-    var currentActive = $('.img-box.active')
-    currentActive.removeClass('active')
-    if (currentActive.hasClass('last')) {
-        $('.img-box.first').addClass('active')
-        $(rad_inputs[$('.img-box.first').attr('value')]).prop("checked", true)
+            currentActive.next().addClass('active')
+            $(rad_inputs[currentActive.next().attr('value')]).prop("checked", true)
+        } 
     }
-    else {
-        currentActive.next().addClass('active')
-        $(rad_inputs[currentActive.next().attr('value')]).prop("checked", true)
-    } 
-}
 
 
-/***************/
-/* MAIN SCRIPT */
-/***************/
-
-// GLOBAL VARIABLES
-const prev_btn = $('#prev-button')
-const next_btn = $('#next-button')
-const img_boxes = $('.img-box')
-const rad_inputs = $('.regular-radio')
-
-// EVENTS
-$(document).ready (function() {
-    // Buttons
-    prev_btn.click(goNext)
-    next_btn.click(goBack)
-
-    // Keyboard
-    $(document).keydown(function(e) {
+    function keyboard() {
         switch (e.keyCode) {
             case 37:
                 goBack()
@@ -60,8 +45,27 @@ $(document).ready (function() {
                 goNext()
                 break
         }
-    })
+    }
 
+
+    /***************/
+    /* MAIN SCRIPT */
+    /***************/
+
+    /* GLOBAL VARIABLES */
+    const rad_inputs = $('.regular-radio')
+    const prev_btn = $('#prev-button')
+    const next_btn = $('#next-button')
+    const img_boxes = $('.img-box')
+    
+
+    /* EVENTS */
+    // Buttons
+    prev_btn.click(goNext)
+    next_btn.click(goBack)
+
+    // Keyboard
+    $(document).keydown(keyboard)
 
     // Controller
     $('.controller label').click(function() {
