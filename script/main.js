b62070d14a2e0ed530f2cf375161b208b203c46a
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-09 16:18:33 +0200

    main task completed

---
+++ script/main.js
@@ -0,0 +1,53 @@
+console.log('main.js is working')
+console.log($)
+
+
+
+
+// GLOBAL VARIABLES
+const prev_btn = $('#prev-button')
+const next_btn = $('#next-button')
+const img_boxes = $('.img-box')
+
+// EVENTS
+prev_btn.click(function (e) { 
+    var currentActive = $('.img-box.active')
+    currentActive.removeClass('active')
+    if (currentActive.hasClass('first')) {
+        $('.img-box.last').addClass('active')
+    }
+    else {
+        currentActive.next().addClass('active')
+    }
+});
+
+
+next_btn.click(function (e) { 
+    var currentActive = $('.img-box.active')
+    currentActive.removeClass('active')
+    if (currentActive.hasClass('last')) {
+        $('.img-box.first').addClass('active')
+    }
+    else {
+        currentActive.next().addClass('active')
+    }
+    
+    
+});
+
+
+/*
+function next_circular (lenght, index, leap=1) {
+    return (index % lenght) + leap - 1
+}
+
+
+const test = [
+    [next_circular(5, 4), 0],
+    [next_circular(5, 3), 4],
+    [next_circular(5, 4, -1), 3],
+    [next_circular(5, 3, -1), 2]
+]
+
+console.table(test) 
+*/
